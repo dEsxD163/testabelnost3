e6d6e6d3ba0c3c50eba5d8ef518eb74aab0862b3
Denis <dzavalskiy@gmail.com>
2023-03-14 18:27:27 +0400

    [200~

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,13 @@ public class Main {
         int term = 12; // кол-во месяцев 12
         int term1 = 24; // кол-во месяцев 24
         int term2 = 36; // кол-во месяцев 36
-        int otvet = service.calculate(remainder, interest, term);
-        int otvet1 = service.calculate(remainder, interest, term1);
-        int otvet2 = service.calculate(remainder, interest, term2);
-        System.out.println("Средний платёж на 12 месяцев:" +otvet+"(рублей)");
-        System.out.println("Средний платёж на 24 месяцев:" +otvet1+"(рублей)");
-        System.out.println("Средний платёж на 36 месяцев:" +otvet2+"(рублей)");
+        int answer = service.calculate(remainder, interest, term); // answer = ответ для 12 месяцев
+        int answer1 = service.calculate(remainder, interest, term1); // answer1 = ответ для 24 месяцев
+        int answer2 = service.calculate(remainder, interest, term2); // answer1 = ответ для 36 месяцев
+        // P.S. Я написал решение для каждого срока отдельно, чтобы потом вывести ответ для всех отдельно.
+        System.out.println("Средний платёж на 12 месяцев:" +answer+"(рублей)");
+        System.out.println("Средний платёж на 24 месяцев:" +answer1+"(рублей)");
+        System.out.println("Средний платёж на 36 месяцев:" +answer2+"(рублей)");
 
     }
 }
